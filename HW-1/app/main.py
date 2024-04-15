cf8115814ec7919a615a0d1b35198bb1e4449f2c
DYDKA4 <hijito@yandex.ru>
2024-04-15 21:10:44 +0300

    HW-2

--- HW-1/app/main.py
+++ HW-1/app/main.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'mysql_db'
+app.config['MYSQL_HOST'] = 'mysqldb'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'example'
 app.config['MYSQL_DB'] = 'stage'
